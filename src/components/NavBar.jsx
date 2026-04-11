b4757f76ba573f6ba82bd9bf4f591864b177c862
Angel Arevalo <angelarevalo480@gmail.com>
2026-04-10 21:21:48 -0400

    Adecuaciones de SEO

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
       <nav className="bg-n5 shadow-md sticky top-0 z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between h-24 items-center">
+          <div className="flex h-24 items-center justify-center md:justify-between">
             <div className="flex-shrink-0 flex items-center">
               <img className="h-16 w-auto" src={logo} alt="Oleoservi C.A." />
             </div>
